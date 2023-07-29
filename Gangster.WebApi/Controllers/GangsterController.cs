1b4aab82348871368234a1af41e22b40b26ced2a
Arun Joshi <arun.joshi@outlook.in>
2023-07-29 20:25:15 +0530

    #comment formatting.

--- Gangster.WebApi/Controllers/GangsterController.cs
+++ Gangster.WebApi/Controllers/GangsterController.cs
@@ -10,7 +10,6 @@ public class GangsterController : ControllerBase
     [HttpGet("Gangsters")]
     public ActionResult<APIResponse<string>> Gangsters()
     {
-        var res = APIResponse<string>.SendResponse(StatusCodes.Status200OK, "this is message", "this is data", true);
-        return res;
+        return APIResponse<string>.SendResponse(StatusCodes.Status200OK, "this is message", "this is data", true);
     }
 }
